2cda896ec9f8dbbe7565d3a4513c752793930b7a
Victor Filgueira <victor.filgueira@mindsatwork.com.br>
2023-07-18 19:23:14 -0300

    feat: init login

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+export const AppRouter: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginModule),
+  },
+];
